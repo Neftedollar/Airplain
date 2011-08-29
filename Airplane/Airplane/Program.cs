cc58d53a915b050a2c9e64cdfc90ebcbacf4fcd9
Semion Laptev <semionlaptev@gmail.com>
2011-08-29 21:56:44 +0400

    beta protype of gameplay, adde games images

--- Airplane/Airplane/Program.cs
+++ Airplane/Airplane/Program.cs
@@ -10,7 +10,7 @@ namespace Airplane
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Airplane game = new Airplane())
             {
                 game.Run();
             }
